1e9cb3885182ff9be1e0eb0f66a9aaacb2f5cb08
Ties Spit <tiesspit@gmail.com>
2026-03-13 14:29:59 +0100

    Added new levels and changed level spawning

--- Assets/Scripts/LevelSpawner.cs
+++ Assets/Scripts/LevelSpawner.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class LevelSpawner : MonoBehaviour
 {
+    [SerializeField] private float spawnDistance;
+
     [SerializeField] private GameObject[] easyLevels;
     [SerializeField] private GameObject[] mediumLevels;
     [SerializeField] private GameObject[] hardLevels;
     [SerializeField] private List<GameObject> playedLevels;
+
     [HideInInspector] public float gameSpeed = 1;
     public static LevelSpawner instance;
 
@@ -22,28 +25,28 @@ public class LevelSpawner : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
 
-        if (playedLevels.Count == 8)
+        if (playedLevels.Count == 9)
         {
             playedLevels.Clear();
             gameSpeed += 0.1f;
         }
 
         //Debug.Log("Spawn");
-        Vector3 spawnPos = spawnPoint.position + (spawnPoint.right * -30);
+        Vector3 spawnPos = spawnPoint.position + (spawnPoint.right * -spawnDistance);
 
         GameObject levelToSpawn = null;
 
-        if (playedLevels.Count >= 0 && playedLevels.Count <= 2)
+        if (playedLevels.Count >= 0 && playedLevels.Count < easyLevels.Length)
         {
-            levelToSpawn = Instantiate(easyLevels[Random.Range(0, easyLevels.Length)], spawnPos, spawnPoint.rotation);
+            levelToSpawn = Instantiate(easyLevels[playedLevels.Count], spawnPos, spawnPoint.rotation);
         }
-        else if (playedLevels.Count >= 3 && playedLevels.Count <= 5)
+        else if (playedLevels.Count >= easyLevels.Length && playedLevels.Count < easyLevels.Length + mediumLevels.Length)
         {
-            levelToSpawn = Instantiate(mediumLevels[Random.Range(0, mediumLevels.Length)], spawnPos, spawnPoint.rotation);
+            levelToSpawn = Instantiate(mediumLevels[playedLevels.Count - easyLevels.Length], spawnPos, spawnPoint.rotation);
         }
         else
         {
-            levelToSpawn = Instantiate(hardLevels[Random.Range(0, hardLevels.Length)], spawnPos, spawnPoint.rotation);
+            levelToSpawn = Instantiate(hardLevels[playedLevels.Count - (easyLevels.Length + mediumLevels.Length)], spawnPos, spawnPoint.rotation);
         }
         
         playedLevels.Add(levelToSpawn);
@@ -52,7 +55,7 @@ public class LevelSpawner : MonoBehaviour
         {
             Transform staticObjectsTransform = levelToSpawn.transform.Find("StaticObjects").transform;
             staticObjectsTransform.parent = GameObject.Find("NewLevelPlatform").transform;
-            //staticObjectsTransform.position = staticObjectsTransform.position + staticObjectsTransform.right * 30;
+            //staticObjectsTransform.position = staticObjectsTransform.position + staticObjectsTransform.right * spawnDistance;
         }
     }
 }
